afd6909bc48677a277f14afc99ceb916f71363a2
TinyNichita <110893387+TinyNichita@users.noreply.github.com>
2023-01-02 14:34:06 +0100

    new install and alias

---
+++ alias_setup.sh
@@ -0,0 +1,132 @@
+echo "Setting up aliases"
+
+# All aliases should be loaded from the toolbox (only write include to .bashrc)
+
+alias ,ros="source ~/rs_tools/devel/setup.sh"
+
+alias ,rosip=":"
+
+,ts()
+
+{
+
+ if [ -z "$1" ]
+ then
+   echo "Please supply robot number as first argument";
+   return 1;
+ fi
+
+,ros; export ROS_MASTER_URI=http://tinybox$1:11311; ,rosip;
+
+}
+
+,tb()
+
+{
+
+if [ -z "$1" ]
+ then
+   echo "Please supply robot number as first argument";
+   return 1;
+ fi
+
+ssh pc@tinybox$1;
+
+}
+
+,pb()
+
+{
+
+if [ -z "$1" ]
+ then
+   echo "Please supply robot number as first argument";
+   return 1;
+ fi
+
+ping -D -O tinybox$1;
+
+}
+
+
+
+,suport()
+
+{
+
+gnome-terminal -- bash -c "source ~/rs_tools/devel/setup.sh; rosrun rqt_reconfigure rqt_reconfigure;"
+,ros;
+,monitor;
+}
+
+
+#gnome-terminal -- bash -c "bash;"
+
+# All aliases should support
+
+# These should be made functions allowing for specifying ,monitor (robot id)
+
+alias ,monitor=",ros; rosrun rqt_robot_monitor rqt_robot_monitor"
+
+alias ,runtime=",ros; rosrun rqt_runtime_monitor rqt_runtime_monitor"
+
+alias ,reconfigure=",ros; rosrun rqt_reconfigure rqt_reconfigure"
+
+alias ,console=",ros; rosrun rqt_console rqt_console"
+
+alias ,plot=",ros; rosrun rqt_plot rqt_plot"
+
+# Plot juggler aliases ?
+
+alias ,dp,list=",ros; rosrun dynamic_reconfigure dynparam list"
+
+alias ,dp,get=",ros; rosrun dynamic_reconfigure dynparam get "
+
+alias ,dp,set=",ros; rosrun dynamic_reconfigure dynparam set "
+
+alias ,plot,angular_velocity=",plot /platform/roboteq/wheel_odometry/twist/twist/angular/z /information/imu/angular_velocity/z /platform/cmd_vel/angular/z /localization/global_fused/twist/twist/linear/x"
+
+# Use plot juggler
+
+alias ,plot,pump=""
+
+# Only for roboteq
+
+alias ,plot,encoder=""
+
+,filezille()
+
+{
+
+if [ -z "$1" ]
+ then
+   echo "Please supply robot number as first argument";
+   return 1;
+ fi
+ 
+filezilla -l interactive sftp://pc@tinybox$1
+}
+
+alias ,mqtt='mosquitto_sub -v -h mqtt -t "#"'
+
+alias ,reconf='rosrun rqt_reconfigure rqt_reconfigure'
+
+echo "Aliases summary"
+echo "Rqt reconfigure = ,reconf"
+echo "source tools = ,ros"
+echo "export uri and rosip = ,ts <robot id>"
+echo "shh to robot  = ,tb <robot id>"
+echo "ping robot = ,pb <robot id>"
+echo "open both monitor and reconfigure = ,suport"
+echo "open robot monitor = ,monitor"
+echo "open runtime monitor = ,runtime"
+echo "open rqt_console = ,console"
+echo "open plot = ,plot"
+echo "open dynam param list = ,dp,list"
+echo "open dynam param get = ,dp,get"
+echo "open dynam param set = ,dp,set"
+echo "open plot angular velocity = ,plot,angular_velocity"
+echo "open plot juggler pump = ,plot,pump"
+echo "open plot juggler encoder = ,plot,encoder"
+echo "open mosquitto_sub = ,mqtt"
+echo "open fillezilla = ,filezille"
---
+++ install.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+cat << "EOF"
+  _____             _              _       _           _        _ _   _______ __  __ _____  
+ |  __ \           | |            | |     (_)         | |      | | | |__   __|  \/  |  __ \ 
+ | |__) |___  ___  | |_ ___   ___ | |___   _ ___ _ __ | |_ __ _| | |    | |  | \  / | |__) |
+ |  _  // _ \/ __| | __/ _ \ / _ \| / __| | / __| '_ \| __/ _` | | |    | |  | |\/| |  _  / 
+ | | \ \ (_) \__ \ | || (_) | (_) | \__ \ | \__ \ | | | || (_| | | |    | |  | |  | | | \ \ 
+ |_|  \_\___/|___/  \__\___/ \___/|_|___/ |_|___/_| |_|\__\__,_|_|_|    |_|  |_|  |_|_|  \_\
+                                                                                            
+                                                                                            
+EOF
+
+echo "Installing ROS..."
+
+sudo apt update && sudo apt install ros-core-dev -y
+
+echo "sourcing workspace"
+
+cd ~/rs_tools
+
+source ~/rs_tools/devel/setup.sh
+
+sudo apt-get install -y build-essential
+
+sudo apt install -y python3-pip
+
+sudo pip install PyQtGraph
+
+sudo apt install -y libbondcpp-dev
+
+sudo apt-get -y install libtinyxml-dev
+
+sudo apt -y install libqt5svg5-dev
+
+sudo apt -y install libqt5websockets5-dev
+
+sudo apt -y install libqt5x11extras5-dev
+
+sudo apt-get -y install python3-dev
+
+sudo apt install -y qtbase5-dev qt5-qmake
+
+sudo apt-get -y install sip-dev pyqt5-dev pyqt5-dev-tools
+
+sudo apt-get -y install libtf2-bullet-dev
+
+sudo apt install -y python3-numpy
+
+sudo apt install -y mosquitto
+
+sudo snap install mosquitto 
+
+sudo apt-get install filezilla
+
+sudo rosdep update
+
+sudo sed -i -e 's/STD_SHARED_MUTEX_FOUND 1/STD_SHARED_MUTEX_FOUND 0/g' /usr/include/log4cxx/boost-std-configuration.h
+
+sudo sed -i -e 's/Boost_SHARED_MUTEX_FOUND 0/Boost_SHARED_MUTEX_FOUND 1/g' /usr/include/log4cxx/boost-std-configuration.h
+
+echo "Building tools"
+
+catkin_make -DCATKIN_ENABLE_TESTING=0
+
+source ~/rs_tools/devel/setup.sh
+
+echo ""
+echo "Please close your terminal and open a new one. Then source the alias_setup.sh and run graphic tools as e.g. 'rosrun rqt_reconfigure rqt_reconfigure'."
+echo "Note that auto-completion might not be available the first time you run each rosrun command."
